b79a2e5161f392481a9b8ea04a17217864a51615
SELBY <70860603+selbykoketso@users.noreply.github.com>
2024-02-02 13:24:04 +0000

    Add CardSales componen

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,45 @@
+import CardFour from '../../components/CardFour.tsx';
+import CardOne from '../../components/CardOne.tsx';
+import CardSales from '../../components/CardSales.tsx';
+import CardThree from '../../components/CardThree.tsx';
+import CardTwo from '../../components/CardTwo.tsx';
+import ChartOne from '../../components/ChartOne.tsx';
+import ChartThree from '../../components/ChartThree.tsx';
+import ChartFour from '../../components/ChartFour.tsx';
+import ChartTwo from '../../components/ChartTwo.tsx';
+import ChatCard from '../../components/ChatCard.tsx';
+import MapOne from '../../components/MapOne.tsx';
+import TableOne from '../../components/TableOne.tsx';
+
+const ECommerce = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardOne />
+        <CardTwo />
+        <CardThree />
+        <CardFour />
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardSales />
+        <CardSales />
+        <CardSales />
+        <CardSales />
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartOne />
+        <ChartTwo />
+        <ChartThree />
+        <ChartFour />
+        <MapOne />
+        <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard />
+      </div>
+    </>
+  );
+};
+
+export default ECommerce;
