a7b06a4e4413e34cc792e7b0e90515abc2e8f1c9
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2024-02-02 11:22:46 +0200

    Basic React Query Api Call is working

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,3 +1,5 @@
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../../common/Loader/index.tsx";
 import CardFour from "../../components/CardFour.tsx";
 import CardOne from "../../components/CardOne.tsx";
 import CardThree from "../../components/CardThree.tsx";
@@ -11,13 +13,38 @@ import TableOne from "../../components/TableOne.tsx";
 import TopProductsCard from "../../components/TopProductsCard.tsx";
 
 const ECommerce = () => {
+  const getStats = async () => {
+    const response = await fetch(
+      "https://j5l5hqnix6.execute-api.af-south-1.amazonaws.com/dev/stats"
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  };
+
+  const { status, isLoading, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getStats,
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (status === "error") {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <CardOne />
         <CardTwo />
         <CardThree />
-        <CardFour />
+        <CardFour
+          totalUsers={data.total_users.amount}
+          changePercentage={data.total_users.change_percent}
+        />
       </div>
 
       <div className="mt-4 col-span-12 xl:col-span-8 md:mt-6 md:gap-6 2xl:mt-7.5">
