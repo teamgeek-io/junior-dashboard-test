40dc152327e23b175945c3db17b23170ba79a2a8
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2024-02-02 12:09:07 +0200

    Card One is showing correct data

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -38,7 +38,10 @@ const ECommerce = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne />
+        <CardOne
+          totalViews={data.total_views.amount}
+          changePercentage={data.total_views.change_percent}
+        />
         <CardTwo
           totalProfit={data.total_profit.amount}
           changePercentage={data.total_profit.change_percent}
