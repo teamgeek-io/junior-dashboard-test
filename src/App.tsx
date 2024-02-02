8d487e5fdc136ba6324020d99d0a077b7a209e6c
milahnmartin <milahnmartin.develop@gmail.com>
2024-02-02 20:30:49 +0200

    fix(Imports):Removed Unused Imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,58 @@
-import { Suspense, lazy, useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { Query, QueryClient,QueryClientProvider,useQuery } from 'react-query';
-import ECommerce from './pages/Dashboard/ECommerce';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import Loader from './common/Loader';
-import routes from './routes';
+import { Suspense, lazy, useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "react-query";
+import ECommerce from "./pages/Dashboard/ECommerce";
+import SignIn from "./pages/Authentication/SignIn";
+import SignUp from "./pages/Authentication/SignUp";
+import Loader from "./common/Loader";
+import routes from "./routes";
 
 const queryClient = new QueryClient();
 
-const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+const DefaultLayout = lazy(() => import("./layout/DefaultLayout"));
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
+	const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 1000);
+	}, []);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <>
-    <QueryClientProvider client={queryClient}>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        containerClassName="overflow-auto"
-      />
-      <Routes>
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-        <Route element={<DefaultLayout />}>
-          <Route index element={<ECommerce />} />
-          {routes.map((routes, index) => {
-            const { path, component: Component } = routes;
-            return (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <Suspense fallback={<Loader />}>
-                    <Component />
-                  </Suspense>
-                }
-              />
-            );
-          })}
-        </Route>
-      </Routes>
-      </QueryClientProvider>
-    </>
-  );
+	return loading ? (
+		<Loader />
+	) : (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<Toaster
+					position="top-right"
+					reverseOrder={false}
+					containerClassName="overflow-auto"
+				/>
+				<Routes>
+					<Route path="/auth/signin" element={<SignIn />} />
+					<Route path="/auth/signup" element={<SignUp />} />
+					<Route element={<DefaultLayout />}>
+						<Route index element={<ECommerce />} />
+						{routes.map((routes, index) => {
+							const { path, component: Component } = routes;
+							return (
+								<Route
+									key={index}
+									path={path}
+									element={
+										<Suspense fallback={<Loader />}>
+											<Component />
+										</Suspense>
+									}
+								/>
+							);
+						})}
+					</Route>
+				</Routes>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
