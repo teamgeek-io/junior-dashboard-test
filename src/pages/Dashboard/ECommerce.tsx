28f51a81f06004c8364a03b4908bfcea32f7eefe
SELBY <70860603+selbykoketso@users.noreply.github.com>
2024-02-03 07:03:53 +0000

    refactor: remove typo

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -19,13 +19,13 @@ const ECommerce = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne />
+        <CardOne />GIT
         <CardTwo />
         <CardThree />
         <CardFour />
       </div>
       <div className="grid grid-cols-1 gap-4 pt-6 md:grid-cols-1 md:gap-6 xl:grid-cols-1 2xl:gap-7.5">
-        <CardSales data={products} />git 
+        <CardSales data={products} /> 
       </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
