79c28e09b0d3237480428b778409a9b831d4b1c9
SELBY <selbykoketso@gmail.com>
2024-02-03 14:22:48 +0000

    remove: typo

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -19,7 +19,7 @@ const ECommerce = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne />GIT
+        <CardOne />
         <CardTwo />
         <CardThree />
         <CardFour />
