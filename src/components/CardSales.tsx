6074bf60411adbeb21f125bf95cab68b3d28fbc2
SELBY <selbykoketso@gmail.com>
2024-02-04 23:38:51 +0000

    remove:

--- src/components/CardSales.tsx
+++ src/components/CardSales.tsx
@@ -8,7 +8,7 @@ const CardSales: React.FC<TableProps> = ({ data }) => {
   return (
     <table className=' border-collapse rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
       <thead>
-        <tr className=' text-left order-collapse border bg-gray-2 dark:bg-meta-4'>
+        <tr className=' text-left order-collapse text-black border border-stroke bg-gray-2 dark:bg-meta-4 dark:text-stroke'>
           <th className=' text-center'>ID</th>
           <th>NAME</th>
           <th>SALES REVENUE</th>
