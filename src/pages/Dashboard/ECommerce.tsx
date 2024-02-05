41a2c9d95aef12eab7fdc6a3c1ceff7cfd07fed4
SELBY <selbykoketso@gmail.com>
2024-02-05 13:30:25 +0000

    feat: sorting functionality

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,16 +1,16 @@
-import CardFour from '../../components/CardFour.tsx';
 import CardOne from '../../components/CardOne.tsx';
-import CardSales from '../../components/CardSales.tsx';
-import CardThree from '../../components/CardThree.tsx';
 import CardTwo from '../../components/CardTwo.tsx';
+import CardThree from '../../components/CardThree.tsx';
+import CardFour from '../../components/CardFour.tsx';
 import ChartOne from '../../components/ChartOne.tsx';
+import ChartTwo from '../../components/ChartTwo.tsx';
 import ChartThree from '../../components/ChartThree.tsx';
 import ChartFour from '../../components/ChartFour.tsx';
-import ChartTwo from '../../components/ChartTwo.tsx';
 import ChatCard from '../../components/ChatCard.tsx';
 import MapOne from '../../components/MapOne.tsx';
 import TableOne from '../../components/TableOne.tsx';
 import jsonData from '../../data.json'
+import TableDisp from '../../components/ProductsTable.tsx';
 
 const ECommerce = () => {
   const products = jsonData.products;
@@ -24,8 +24,8 @@ const ECommerce = () => {
         <CardThree />
         <CardFour />
       </div>
-      <div className="grid grid-cols-1 gap-4 pt-6 md:grid-cols-1 md:gap-6 xl:grid-cols-1 2xl:gap-7.5">
-        <CardSales data={products} /> 
+      <div className="grid grid-cols-1 gap-5 pt-6 md:grid-cols-1 md:gap-6 xl:grid-cols-1 2xl:gap-7.5">
+        <TableDisp />
       </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
